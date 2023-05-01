26c5ebc975a142b11b57de03b457d062bd044d98
JGuven <joseph.guven@gmail.com>
2023-05-01 21:02:03 +0100

    - Added buttons to ingredients screen ( not finished )

--- fridge_app/lib/Ingredients.dart
+++ fridge_app/lib/Ingredients.dart
@@ -18,6 +18,55 @@ class Ingredients extends StatelessWidget {
         centerTitle: true,
         backgroundColor: const Color.fromARGB(255, 82, 109, 209),
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                ElevatedButton(
+                  style: buttonStyle,
+                  onPressed: () {},
+                  child: const Text('Add'),
+                ),
+                ElevatedButton(
+                  style: buttonStyle,
+                  onPressed: () {},
+                  child: const Text('Filter A-Z'),
+                ),
+                ElevatedButton(
+                  style: buttonStyle,
+                  onPressed: () {},
+                  child: const Text('Filter by date'),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                const Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Search',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            Expanded(
+              child: Container(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
