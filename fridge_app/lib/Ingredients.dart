2f2c4239d26d33949a0dead374b031e880a90857
OwenMonckton <owen_monckton@yahoo.co.uk>
2023-05-01 20:41:06 +0100

    start on ingredients

--- fridge_app/lib/Ingredients.dart
+++ fridge_app/lib/Ingredients.dart
@@ -3,14 +3,20 @@ import 'package:flutter/material.dart';
 class Ingredients extends StatelessWidget {
   const Ingredients({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Ingredients'),
+        toolbarHeight: 100,
+        title: const Text(
+          'Ingredients',
+          style: TextStyle(
+            fontFamily: 'CartoonistHand',
+            fontSize: 32,
+          ),
+        ),
         centerTitle: true,
-        backgroundColor: Colors.blue[600],
+        backgroundColor: const Color.fromARGB(255, 82, 109, 209),
       ),
     );
   }
